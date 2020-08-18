c326545a6f6749203550d11c2d4b987a893f486f
jiale <jiale8432@gmail.com>
2020-08-18 12:52:35 +0800

    arrayList and interfaceList update

--- assignment/src/GameObject/ArrayList.java
+++ assignment/src/GameObject/ArrayList.java
@@ -7,202 +7,108 @@ package GameObject;
 
 //import java.util.Iterator;
 //import java.util.NoSuchElementException;
-
 /**
  *
  * @author CHONGJL
  * @param <T>
- 
-
-
+ *
+ *
+ *
+ *
  */
-public class ArrayList<T> implements List<T>{
+public class ArrayList<T> implements List<T> {
 
-   //private int size;
+    //private int size;
     public int length;
     private T[] array;
-    private static int DEFAULT_CAPACITY=5;
-  //  private String objectName;
-    
-    
-    public ArrayList(){
-    this(DEFAULT_CAPACITY);
+    private static int DEFAULT_CAPACITY = 5;
+    //  private String objectName;
+
+    public ArrayList() {
+        this(DEFAULT_CAPACITY);
     }
-    public ArrayList(int initialCapacity){
-        length=0;
-    //   size=0;
-        array=(T[]) new Object[initialCapacity];
-    
+
+    public ArrayList(int initialCapacity) {
+        length = 0;
+        //   size=0;
+        array = (T[]) new Object[initialCapacity];
+
     }
-    
 
     //add back
-        @Override
-    public void add(T newObject){
-        if(this.length== array.length){
+    @Override
+    public void add(T newObject) {
+        if (this.length == array.length) {
             addArrayLength();
         }
-        array[length]=newObject;
+        array[length] = newObject;
         length++;
     }
-    
-    public int length(){
+
+    public int length() {
         return array.length;
     }
-    /*
-   public void addFirst(T newObject){
-   
-   }
-   public void addMiddle(int position, T newObject){
-   
-   }
-*/
-     public T getEntry(int givenPosition) {
-    T result = null;
 
-    if ((givenPosition >= 1) && (givenPosition <= length)) {
-      result = array[givenPosition - 1];
+    public T getEntry(int givenPosition) {
+        T result = null;
+
+        if ((givenPosition >= 1) && (givenPosition <= length)) {
+            result = array[givenPosition - 1];
+        }
+
+        return result;
     }
 
-    return result;
-  }
-    private void addArrayLength(){
-        T[] oldArray=array;
-        int temArrayLength= array.length;
-        T[] temArray=(T[]) new Object[temArrayLength * 2];
-            for(int i=0; i< oldArray.length;i++){
-                temArray[i]=array[i];
+    private void addArrayLength() {
+        T[] oldArray = array;
+        int temArrayLength = array.length;
+        T[] temArray = (T[]) new Object[temArrayLength * 2];
+        for (int i = 0; i < oldArray.length; i++) {
+            temArray[i] = array[i];
+        }
+        array = temArray;
+    }
+
+    @Override
+    public T remove(int givenPosition) {
+        T result = null;
+
+        if ((givenPosition >= 1) && (givenPosition <= length)) {
+            result = array[givenPosition - 1];
+
+            if (givenPosition < length) {
+                removeGap(givenPosition);
             }
-    array=temArray;
+
+            length--;
+        }
+
+        return result;   //To cha//To change body of generated methods, choose Tools | Templates.
     }
 
+    private void removeGap(int givenPosition) {
+        // move each entry to next lower position starting at entry after the
+        // one removed and continuing until end of array
+        int removedIndex = givenPosition - 1;
+        int lastIndex = length - 1;
 
-    //remove behide object
+        for (int index = removedIndex; index < lastIndex; index++) {
+            array[index] = array[index + 1];
+        }
+    }
 
-  private void removeGap(int givenPosition) {
-    // move each entry to next lower position starting at entry after the
-    // one removed and continuing until end of array
-    int removedIndex = givenPosition - 1;
-    int lastIndex = length - 1;
-
-    for (int index = removedIndex; index < lastIndex; index++) {
-      array[index] = array[index + 1];
-    }
-  }
-    /*
-    public T remove(int position){
-    
-    }
-    public void remove(T data){
-    
-    }
-    
-    public void replace(int position){
-    
-    }
-*/
-    
-
+    @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        return length == 0;
     }
 
-
-    public String findGameObject() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    public  String toString(){
-        String outPutGameObject="";
-        for(int size=0; size<length;++size){
-        outPutGameObject+=array[size];
+    public String toString() {
+        String outPutGameObject = "";
+        for (int size = 0; size < length; ++size) {
+            outPutGameObject += array[size];
         }
         return outPutGameObject;
     }
 
-
-
-/*
-    @Override
-    public Iterator<T> getIterator() {
-        return new IteratorForArrayList();
-    }
-    */
-    /*
-    private class IteratorForArrayList implements Iterator<T> {
-
-    private int nextIndex;
-    private boolean wasNextCalled; // needed by remove
-
-    private IteratorForArrayList() {
-      nextIndex = 0;
-      wasNextCalled = false;
-    }
-
-
-    public boolean hasNext() {
-      return nextIndex < length;
-    }
-
-
-    public T next() {
-      if (hasNext()) {
-        wasNextCalled = true;
-        T nextEntry = array[nextIndex];
-        nextIndex++; // advance iterator
-
-        return nextEntry;
-      } else {
-        throw new NoSuchElementException("Illegal call to next();"
-                + "iterator is after end of list.");
-      }
-    }
-/*
-    public void remove() {
-      if (wasNextCalled) {
-        // nextIndex was incremented by the call to next, so it 
-        // is the position number of the entry to be removed
-        GameObjectList.this.remove(nextIndex);
-        nextIndex--;           // index of next entry in iteration
-        wasNextCalled = false; // reset flag
-      } else {
-        throw new IllegalStateException("Illegal call to remove(); "
-                + "next() was not called.");
-      }
-    }
-    */
-   // }
-/*
-    @Override
-    public Iterator<T> getIterator() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-*/
-
-
-    public T remove(int givenPosition) {
-   T result = null;
-
-    if ((givenPosition >= 1) && (givenPosition <= length)) {
-      result = array[givenPosition - 1];
-
-      if (givenPosition < length) {
-        removeGap(givenPosition);
-      }
-
-      length--;
-    }
-
-    return result;   //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public T remove() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-
-
-  
-        
 }
--- assignment/src/GameObject/List.java
+++ assignment/src/GameObject/List.java
@@ -7,23 +7,23 @@ import java.util.Iterator;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author CHONGJL
- *   @param <T>
+ * @param <T>
  */
 public interface List<T> {
-    
- //   Iterator<T> getIterator() ;
-    void add(T gameObjectName);
-    T remove();
-    T getEntry(int givenPosition);
-    boolean isEmpty();
-    String findGameObject();
-    int length();
-  //  int length();
 
-   
-    
+    //   Iterator<T> getIterator() ;
+    void add(T gameObjectName);
+
+    T remove(int givenPosition);
+
+    T getEntry(int givenPosition);
+
+    boolean isEmpty();
+
+    int length();
+    //  int length();
+
 }
