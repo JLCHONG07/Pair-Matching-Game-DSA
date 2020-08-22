65ffd96aa778623cda63d024246f08f81980251f
jiale <jiale8432@gmail.com>
2020-08-22 23:17:45 +0800

    higesht score

--- assignment/src/GamePlay/GamePlay.java
+++ assignment/src/GamePlay/GamePlay.java
@@ -13,6 +13,7 @@ import Level.LVL;
 import static Level.LVL.*;
 import Player.PromptPlayer;
 
+
 import static assignment.Assignment.MainMenu;
 
 import java.util.Scanner;
@@ -30,7 +31,7 @@ public class GamePlay {
     String objPaired = "";
     public static int totalRounds = 10;// changed base on level getRounds();
     int sum = 0;
-    int highestScore = 0;
+   int highestScore = 0;
     int currentScore = 0;
     public static int rounds = 1;
     public static int countingPair = 0;
@@ -59,28 +60,28 @@ public class GamePlay {
         if (!score.isEmpty()) {
             score.clear();
         }
-        
-        player.assignHighestScore();
-        
-        score.push(new PlayGame(currentRounds, currentLvlScore));
 
-        System.out.println("Your Highest Scores : " + player.highestScore() + "\n");
+        player.assignHighestScore();
+
+        score.push(new PlayGame(currentRounds, currentLvlScore));
+        
+        highestScore= player.highestScore();
+        System.out.println("Your Highest Scores : " + highestScore + "\n");
 
         System.out.print("Round :" + rounds + "/" + score.pop().getRound());
 
         System.out.print("\n\nObjects You Have Paired : " + objPaired);
 
         //System.out.print("\nObjects You Have Paired : " + objPaired);
-
         System.out.print("\n\nObj 1 :");
-        
-        do{
-            if(isChecked == false) {
+
+        do {
+            if (isChecked == false) {
                 System.out.print("\n\nEnter Obj 1 Again :");
             }
             decision1 = scan.nextLine();
             isChecked = inputCheck(decision1);
-        }while(isChecked == false);
+        } while (isChecked == false);
 
         decisionConvert1 = Integer.parseInt(decision1);
         while (go.getEntry(decisionConvert1).getIsPairs() == true) {
@@ -93,19 +94,19 @@ public class GamePlay {
         }
         System.out.print(openDot + TisObj + go.getEntry(decisionConvert1).getDescription() + closeDot + "\n");
         obj1Choosed = go.getEntry(decisionConvert1).getObjID();
-        
+
         System.out.print("\n\nObj 2 :");
-        
-        do{
-            if(isChecked == false) {
+
+        do {
+            if (isChecked == false) {
                 System.out.print("\n\nEnter Obj 2 Again :");
             }
             decision2 = scan.nextLine();
             isChecked = inputCheck(decision2);
-        }while(isChecked == false);
-        
+        } while (isChecked == false);
+
         decisionConvert2 = Integer.parseInt(decision2);
-        
+
         while (go.getEntry(decisionConvert2).getIsPairs() == true || decision2.equals(decision1)) {
             if (go.getEntry(decisionConvert2).getIsPairs() == true) {
                 System.out.print("\n\nThis object has been added before! :");
@@ -133,7 +134,7 @@ public class GamePlay {
 
             go.getEntry(decisionConvert1).setIsPairs(true);
             go.getEntry(decisionConvert2).setIsPairs(true);
-            
+
             score.push(new PlayGame(currentRounds, currentLvlScore));
             while (!score.isEmpty()) {
                 sum += score.pop().getScore();
@@ -145,11 +146,12 @@ public class GamePlay {
             /*
              * score.push(10); while (score.size() > 0) { sum += score.pop(); currentScore =
              * sum; }
-             */
-
-            System.out.println("Current Score : " + currentScore);
+     //        */
+        //    System.out.println("Current Score : " + currentScore);
             if (currentScore > highestScore) {
-                highestScore = currentScore;
+             highestScore= currentScore;
+               player.saveTotalResults(highestScore);
+                 System.out.println("Highest Score Updated : " +   highestScore);
             }
 
         } else {
@@ -160,7 +162,7 @@ public class GamePlay {
                 System.out.println(sum);
                 currentScore = sum;
             }
-           
+
             System.out.println("\nCurrent Score Remainded : " + sum + "\n\n");
 
         }
@@ -177,7 +179,7 @@ public class GamePlay {
         } else if (countingPair == max) {
             int bonus = 0;
             bonus = (totalRounds - rounds) * 10;
-            totalScoresEarn = bonus + currentScore;
+            totalScoresEarn = bonus + highestScore;//or highestScore
             System.out.print("Congratulation you have found all the pairs!\n");
             System.out.print("Current Scores is : " + currentScore + "\n");
             System.out.print("Your Bonus is :  " + bonus + "\n");
@@ -188,31 +190,29 @@ public class GamePlay {
 
         }
         pressAnyKeyToContinue();
-        
+
     }
-    
-     private void pressAnyKeyToContinue()
- { 
+
+    private void pressAnyKeyToContinue() {
         System.out.println("Press Enter key to continue...");
-        try
-        {
+        try {
             System.in.read();
-        }  
-        catch(Exception e)
-        {}  
- }
-    
-        public void Again(int score) {
+        
+        } catch (Exception e) {
+        }
+    }
+
+    public void Again(int score) {
         GameObject obj = new GameObject();
         LVL difficulty = new LVL();
         Scanner scan = new Scanner(System.in);
         PromptPlayer player = new PromptPlayer();
-        
+
         objNo = 0;
         rounds = 1;
         countingPair = 0;
         gamePlayleft = true;
-        
+
         player.saveTotalResults(score);
         System.out.print("GAME OVER !\n");
         System.out.println("Play Again? (Y/N) : ");
@@ -220,43 +220,44 @@ public class GamePlay {
         if (choice == 'Y' || choice == 'y') {
             // go.clear();
             // again = true;
-            
-                 difficulty.LevelDecision();
-               obj.randomObj(noOfGameObject);
+
+            difficulty.LevelDecision();
+            obj.randomObj(noOfGameObject);
             // countingPair=0;
         } else {
             MainMenu();
         }
 
     }
-    
+
     public boolean inputCheck(String decision) {
         boolean isBool = true;
         int tempDecision;
-        if(decision.isEmpty()) {
+        if (decision.isEmpty()) {
             System.out.print("\nPlease enter a valid input ! ");
             System.out.print("\n\nPlease Enter Obj 2 Again :");
-        } else if(!decision.isEmpty()) {
-            if((decision.matches("^[a-zA-Z]*$"))) {
+            isBool = false;
+        } else {
+            if ((decision.matches("^[a-zA-Z]*$"))) {
                 System.out.println("\nPlease enter number !");
                 isBool = false;
             } else {
                 tempDecision = Integer.parseInt(decision);
-                switch(currentLevel) {
+                switch (currentLevel) {
                     case "L01":
-                        if(tempDecision > 9) {
+                        if (tempDecision > 9) {
                             System.out.print("\nPlease enter valid choice !");
                             isBool = false;
                         }
                         break;
                     case "L02":
-                        if(tempDecision > 13) {
+                        if (tempDecision > 13) {
                             System.out.print("\nPlease enter valid choice !");
                             isBool = false;
                         }
                         break;
                     case "L03":
-                        if(tempDecision > 17) {
+                        if (tempDecision > 17) {
                             System.out.print("\nPlease enter valid choice !");
                             isBool = false;
                         }
